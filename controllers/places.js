89682e8a888fef8d87331d39006bdab606ee31ac
Laith Albusairi <laithalbusairi@gmail.com>
2022-09-18 11:07:34 -0700

    added new.jsx

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,27 @@
 const router = require('express').Router()
-
-//More code in a moment
-
+const places = require('../models/places.js')
+//get the route that will eventually show the list of places
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thai-cafe.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/cat-rest.jpg'
-      }]
-      
-      res.render('places/index', { places })
-
+    res.render('places/index', { places })
 })
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
 
 module.exports = router
