bd2a4c936a2ad03167a977d868815c708866092a
Laith Albusairi <laithalbusairi@gmail.com>
2022-09-20 07:03:41 -0700

    adjusted pics

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,7 +8,7 @@ function error404 () {
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
               <div>
-  <img src="/images/ghost.jpg" alt="ghost cookie"></img>
+  <img src="/images/ghost.jpg" alt="ghost cookie"width="40%"/>
 </div>
 <div>
   Photo by <a href="AUTHOR_LINK">Anna Tukhfatullina</a> on<a href="UNSPLASH_LINK">Food Photographer/Stylist</a>
